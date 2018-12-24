5b548ecd59bcd63efc6b0f5091f4a0e84cc5c759
Shaun <shaunyuan@foxmail.com>
2018-12-24 22:32:21 +0800

    Add tag style

--- scripts/codeblock.js
+++ scripts/codeblock.js
@@ -15,7 +15,7 @@ hexo.extend.filter.register('after_post_render', function (data) {
         data.content = data.content.replace(/<figure class="highlight ([a-zA-Z]+)">.*?<\/figure>/, function () {
             var language = RegExp.$1 || 'CODE'
             var lastMatch = RegExp.lastMatch
-            lastMatch = lastMatch.replace(/<figure class="highlight/, '<figure class="highlight ')
+            lastMatch = lastMatch.replace(/<figure class="highlight/, '<figure class="highlight hljs')
             return '<div class="highlight-wrap"' + attributesStr + 'data-lang="' + language.toUpperCase() + '">' + lastMatch + '</div>'
         })
     }
